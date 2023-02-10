2a931510e7602e4891e61b822d03bc1343972ceb
David Marr <david.marr@contextlabs.com>
2023-02-09 22:57:27 -0500

    Use existing searchParams object

--- server/routes/logout.ts
+++ server/routes/logout.ts
@@ -1,10 +1,6 @@
 export default eventHandler(event => {
   const config = useRuntimeConfig();
   const logoutUrl = new URL('/v2/logout?', config.auth0Issuer);
-  const logoutUrlParams = new URLSearchParams({
-    client_id: config.auth0ClientId,
-    // returnTo: config.public.siteUrl
-  })
-
-  return sendRedirect(event, logoutUrl + logoutUrlParams.toString());
+  logoutUrl.searchParams.append('client_id', config.auth0ClientId);
+  return sendRedirect(event, logoutUrl.toString());
 });
